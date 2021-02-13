b2847eedac2f3e65b4050c637ed724ad90dd947e
Filipe Silva <filipeqs@outlook.com>
2021-02-13 07:41:34 -0500

    fetching content

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
@@ -7,21 +6,14 @@ class App extends Component {
         super();
 
         this.state = {
-            monsters: [
-                {
-                    name: 'Frankenstein',
-                    id: 'a1',
-                },
-                {
-                    name: 'Dracula',
-                    id: 'b2',
-                },
-                {
-                    name: 'Zombie',
-                    id: 'c3',
-                },
-            ],
+            monsters: [],
         };
+    }
+
+    componentDidMount() {
+        fetch('https://jsonplaceholder.typicode.com/users')
+            .then((response) => response.json())
+            .then((users) => this.setState({ monsters: users }));
     }
 
     render() {
